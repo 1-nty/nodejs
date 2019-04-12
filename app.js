9a861fa25b585952c59076107472c3f415fc2848
1-nty <46821853+1-nty@users.noreply.github.com>
2019-04-12 10:37:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+'use strict'
+var express = require('express')
+var app = express()
+app.get('/',(req,res)=>{
+  res.status(200).sendFile(__dirname+'/index.html');
+});
+var server = app.listen(process.env.PORT || '8080',function(){
+  console.log('App listening on port %s',server.address().port);
+  console.log('Press Ctrl+C to quit');
+})
